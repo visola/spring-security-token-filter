b2d27619b0e3bc38be67a46c7cc461bc3aa1b038
Vinicius Isola <viniciusisola@gmail.com>
2015-12-04 15:05:17 -0500

    Make roles attribute an array

--- src/main/java/org/visola/spring/security/tokenfilter/jwt/UsernamePasswordAuthenticationTokenJwtClaimsSetTransformer.java
+++ src/main/java/org/visola/spring/security/tokenfilter/jwt/UsernamePasswordAuthenticationTokenJwtClaimsSetTransformer.java
@@ -1,7 +1,6 @@
 package org.visola.spring.security.tokenfilter.jwt;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +19,7 @@ import com.nimbusds.jwt.JWTClaimsSet;
 public class UsernamePasswordAuthenticationTokenJwtClaimsSetTransformer implements AuthenticationJwtClaimsSetTransformer {
 
   private static final String EMPTY_PASSWORD = "";
+  private static final String ROLES_FIELD = "roles";
 
   private final long tokenDuration;
   private final Optional<String> rolePrefix;
@@ -35,35 +35,39 @@ public class UsernamePasswordAuthenticationTokenJwtClaimsSetTransformer implemen
     User user = (User) auth.getPrincipal();
     long now = System.currentTimeMillis();
 
-    StringBuilder roles = new StringBuilder();
-    for (GrantedAuthority authority : auth.getAuthorities()) {
-      String role = authority.getAuthority();
-      if (rolePrefix.isPresent()) {
-        role = role.substring(rolePrefix.get().length(), role.length());
-      }
-      roles.append(role.toLowerCase());
-      roles.append(",");
-    }
-    roles.setLength(roles.length() - 1);
+    List<String> roles = auth.getAuthorities().stream()
+      .map(a -> {
+        String role = a.getAuthority();
+        if (rolePrefix.isPresent()) {
+          role = role.substring(rolePrefix.get().length(), role.length());
+        }
+        return role;
+      })
+      .collect(Collectors.toList());
 
     return new JWTClaimsSet.Builder()
         .subject(user.getUsername())
         .issueTime(new Date(now))
         .expirationTime(new Date(now + tokenDuration))
-        .claim("roles", roles.toString())
+        .claim(ROLES_FIELD, roles)
         .build();
   }
 
   @Override
-  public Authentication getAuthentication(JWTClaimsSet claimSet) {
-    List<? extends GrantedAuthority> authorities = new ArrayList<>();
-    String roles = (String) claimSet.getClaim("roles");
+  public Authentication getAuthentication(JWTClaimsSet claimsSet) {
+    List<? extends GrantedAuthority> authorities = getAuthorities(claimsSet);
+    return new UsernamePasswordAuthenticationToken(claimsSet.getSubject(), EMPTY_PASSWORD, authorities);
+  }
+
+  @SuppressWarnings("unchecked")
+  private List<? extends GrantedAuthority> getAuthorities(JWTClaimsSet claimsSet) {
+    List<String> roles = (List<String>) claimsSet.getClaim(ROLES_FIELD);
     if (roles != null) {
-      authorities = Arrays.asList(roles.replaceAll("\\s*,\\s*", ",").split(",")).stream()
+      return roles.stream()
           .map(role -> new SimpleGrantedAuthority(rolePrefix.orElse("")+role.toUpperCase()))
           .collect(Collectors.toList());
     }
-    return new UsernamePasswordAuthenticationToken(claimSet.getSubject(), EMPTY_PASSWORD, authorities);
+    return new ArrayList<>();
   }
 
 }
