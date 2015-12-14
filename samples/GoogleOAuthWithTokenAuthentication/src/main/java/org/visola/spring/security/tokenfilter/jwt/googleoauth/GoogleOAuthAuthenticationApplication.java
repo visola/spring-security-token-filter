142ea48094da9661d73be95f2aede6c004d00dd3
Vinicius Isola <viniciusisola@gmail.com>
2015-12-13 20:58:47 -0500

    Intial Google OAuth project structure

---
+++ samples/GoogleOAuthWithTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/googleoauth/GoogleOAuthAuthenticationApplication.java
@@ -0,0 +1,9 @@
+package org.visola.spring.security.tokenfilter.jwt.googleoauth;
+
+public class GoogleOAuthAuthenticationApplication {
+
+  public static void main (String [] args) {
+    System.out.println("Test");
+  }
+
+}
