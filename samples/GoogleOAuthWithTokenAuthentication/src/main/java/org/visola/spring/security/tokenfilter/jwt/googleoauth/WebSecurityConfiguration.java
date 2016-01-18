f32414910dbad184336d65fe59ae4b25a5e573d6
Vinicius Isola <visola@hubspot.com>
2016-01-18 11:45:34 -0500

    Move HttpSecurity configuration outside library

--- samples/GoogleOAuthWithTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/googleoauth/WebSecurityConfiguration.java
+++ samples/GoogleOAuthWithTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/googleoauth/WebSecurityConfiguration.java
@@ -1,24 +1,36 @@
 package org.visola.spring.security.tokenfilter.jwt.googleoauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.visola.spring.security.tokenfilter.TokenAuthenticationFilter;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private TokenAuthenticationFilter tokenAuthenticationFilter;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.addFilterBefore(tokenAuthenticationFilter, BasicAuthenticationFilter.class);
+
     http
-    .authorizeRequests()
+      .authorizeRequests()
 
-    // Authenticate endpoint can be access by anyone
-    .antMatchers("/login.html").anonymous()
+      // Authenticate endpoint can be access by anyone
+      .antMatchers("/login.html").anonymous()
 
-    // Secure the endpoints
-    .antMatchers("/api/v1/*").hasAnyRole("USER");
+      // Secure the endpoints
+      .antMatchers("/api/v1/*").hasAnyRole("USER");
   }
 
 }
--- samples/SimpleTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/samples/WebSecurityConfiguration.java
+++ samples/SimpleTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/samples/WebSecurityConfiguration.java
@@ -1,16 +1,28 @@
 package org.visola.spring.security.tokenfilter.jwt.samples;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.visola.spring.security.tokenfilter.TokenAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private TokenAuthenticationFilter tokenAuthenticationFilter;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.addFilterBefore(tokenAuthenticationFilter, BasicAuthenticationFilter.class);
+
     http
       .authorizeRequests()
 
