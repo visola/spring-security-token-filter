0e0d2f12e245ee38c89ce2dcdcff4e6bae073d9e
Vinicius Isola <viniciusisola@gmail.com>
2018-05-06 11:17:12 -0400

    Upgrade to Spring Boot 2

--- samples/SimpleTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/samples/GlobalSecurityConfiguration.java
+++ samples/SimpleTokenAuthentication/src/main/java/org/visola/spring/security/tokenfilter/jwt/samples/GlobalSecurityConfiguration.java
@@ -3,7 +3,7 @@ package org.visola.spring.security.tokenfilter.jwt.samples;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 
 @Configuration
 public class GlobalSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
